5ab0b6ede0f108ad6782e697bc485ce826b3c051
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2022-06-07 16:20:00 -0700

    deploy photo ecommerce to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+
+function App() {    
+    return (
+        <div>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Photos/>} />
+                <Route path="/cart" element={<Cart/>} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+    )
 }
 
-export default App;
+export default App
